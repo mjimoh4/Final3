f1e42b94927448e78a16dc0f1720d44b3ec71f6c
mjimoh4 <mjimoh@montgomerycollege.edu>
2019-08-23 14:55:23 -0400

    final part 3

--- src/main/java/com/example/demo/Message.java
+++ src/main/java/com/example/demo/Message.java
@@ -5,9 +5,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
 
 @Entity
 public class Message
@@ -20,8 +20,9 @@ public class Message
     @Size(min=10, max=100)
     private String content;
 
+    private String image;
 
-    private int postedDate;
+    private String postedDate;
 
     @NotNull
     @Size(min=2)
@@ -44,11 +45,19 @@ public class Message
         this.content = content;
     }
 
-    public int getPostedDate() {
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getPostedDate() {
         return postedDate;
     }
 
-    public void setPostedDate(int postedDate) {
+    public void setPostedDate(String postedDate) {
         this.postedDate = postedDate;
     }
 

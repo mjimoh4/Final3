fb367ff15fdd4ee4431db73545feccde7b18aa47
mjimoh4 <mjimoh@montgomerycollege.edu>
2019-08-23 09:39:47 -0400

    FInal one part 1

---
+++ src/main/java/com/example/demo/FInalApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FInalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FInalApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/demo/Message.java
@@ -0,0 +1,63 @@
+
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Message
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min=4)
+    private String content;
+
+    @NotNull
+    @Min(3)
+    private int postedDate;
+
+    @NotNull
+    @Size(min=10)
+    private String sentBy;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getPostedDate() {
+        return postedDate;
+    }
+
+    public void setPostedDate(int postedDate) {
+        this.postedDate = postedDate;
+    }
+
+    public String getSentBy() {
+        return sentBy;
+    }
+
+    public void setSentBy(String sentBy) {
+        this.sentBy = sentBy;
+    }
+}
